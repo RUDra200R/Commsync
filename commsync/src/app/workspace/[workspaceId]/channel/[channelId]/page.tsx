c9f982978e52124df8ce97de8ea444dec255b1f2
RUDra200R <127923748+RUDra200R@users.noreply.github.com>
2025-03-02 13:54:58 +0530

    Add files via upload

--- commsync/src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ commsync/src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -7,13 +7,20 @@ import { useChannelId } from "@/hooks/use-channel-id";
 import { Loader, TriangleAlert } from "lucide-react";
 
 import { Header } from "./Header";
+import { ChatInput } from "./chat-input";
+import { useGetMessages } from "@/features/messages/api/use-get-messages";
+import { MessageList } from "@/components/message-list";
 
 const ChannelIdPage = () =>{
 
     const channelId = useChannelId();
+    const { results, status, loadMore } = useGetMessages({channelId})
 
     const {data: channel, isLoading: channelLoading} = useGetChannel({id: channelId});
-    if(channelLoading)
+    
+    console.log({results});
+
+    if(channelLoading || status ==="LoadingFirstPage")
     {
         return (
             <div className="h-full flex-1 flex items-center justify-center">
@@ -35,6 +42,15 @@ const ChannelIdPage = () =>{
     return (
         <div className="flex flex-col h-full">
             <Header title={channel.name}/>
+            <MessageList
+            channelName={channel.name}
+            channelCreationTime={channel._creationTime}
+            data={results}
+            loadMore={loadMore}
+            isLoadingMore={status === "LoadingMore"}
+            canLoadMore={status ==="CanLoadMore" }
+            />
+            <ChatInput placeholder={`Message # ${channel.name}`} />
         </div>
     );
 };
