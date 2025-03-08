9a005ca17f1c781963f5ab4e25fda7d7a9d261e7
RUDra200R <127923748+RUDra200R@users.noreply.github.com>
2025-03-08 16:35:57 +0530

    Add files via upload

--- commsync/convex/messages.ts
+++ commsync/convex/messages.ts
@@ -18,6 +18,7 @@ const populateThread = async(ctx: QueryCtx, messageId: Id<"messages">) =>{
             count: 0,
             image: undefined,
             timeStamp: 0,
+            name: "",
         };
     }
 
@@ -29,6 +30,7 @@ const populateThread = async(ctx: QueryCtx, messageId: Id<"messages">) =>{
             count: 0,
             image: undefined,
             timeStamp: 0,
+            name: "",
         };
     }
 
@@ -38,6 +40,7 @@ const populateThread = async(ctx: QueryCtx, messageId: Id<"messages">) =>{
         count: messages.length,
         image: lastMessageUser?.image,
         timeStamp: lastMessage._creationTime,
+        name: lastMessageUser?.name,
 
     };       
     
@@ -318,6 +321,7 @@ export const get = query({
                         reactions: reactionWithoutMemberIdProperty,
                         threadCount: thread.count,
                         threadImage: thread.image,
+                        threadName: thread.name,
                         threadTimestamp: thread.timeStamp,
                     };
 
